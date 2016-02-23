b1f9ba8925747d37dbed5dfc3ef52c73a51f8a6c
Jeeva Nadarajah <jeeva.ganeshan@gmail.com>
2016-02-23 09:27:21 -0500

    added some tests

--- vendingMachine.py
+++ vendingMachine.py
@@ -1,3 +1,5 @@
+# from led import LED
+
 #Valid Coins
 NICKEL = "nickel"
 DIME = "dime"
@@ -18,21 +20,26 @@ class VendingMachine:
         self.products = {APPLE: 0.65, ORANGE: 0.75, BANANA: 1.0}
 
     def input_coin(self, coin):
-        # if coin in self.valid_coins:
-            self.coins.append(coin)
+        if coin in self.valid_coins:
+            self.add_coins(coin)
+
+    def add_coins(self, coin):
+        self.coins.append(coin)
+
+
+    def get_coins_value(self):
+        value = 0
+        for index in range(len(self.coins)):
+            coin = self.coins[index]
+            cents = self.valid_coins[coin]
+            value += cents
+        return value
 
     def can_purchase(self, selected_product):
         if self.get_coins_value() >= self.products[selected_product]:
+            # LED.blink(2,1)
             return True
         return False
-
-    def get_coins_value(self):
-        cash = 0
-        for index in range(len(self.coins)):
-            coin = self.coins[index]
-            money = self.valid_coins[coin]
-            cash += money
-        return cash
 
     def get_products(self):
         return self.products
@@ -40,6 +47,7 @@ class VendingMachine:
     def purchase_product(self, product):
         if product in self.products:
             self.coins = []
+
             return "Purchased {}".format(product)
 
     def get_product_price(self, product):
--- vendingMachineTest.py
+++ vendingMachineTest.py
@@ -1,20 +1,42 @@
-#Write a test to validate the inserted coin is in the list
+#Write a test for input_coin to make sure add_coins is not called when an invalid coin is entered
+#Write a test for input_coin to make sure add_coins is called when a valid coin is entered
+#Write a test for input_coin to make sure t
+
+#Refactor
+
 
 import mock
 import unittest
-from nose import  tools as nt
+from nose import tools as nt
 from vendingMachine import VendingMachine
 
 class VendingMachineTest(unittest.TestCase):
+    def setUp(self):
+        self.vm = VendingMachine()
 
-    @mock.patch('append()')
-    def test_input_coin_valid_coin(self, append_):
-        vm = VendingMachine()
-        vm.valid_coins = {'a': 1, 'b': 2}
+    @mock.patch('vendingMachine.VendingMachine.add_coins')
+    def test_add_coins_should_not_be_called_when_coin_is_invalid(self, add_coins_):
+        self.vm.valid_coins = {'a': 1, 'b': 2}
 
-        vm.input_coin('c')
+        self.vm.input_coin('c')
 
-        assert not append_.called()
+        assert not add_coins_.called
+
+    @mock.patch('vendingMachine.VendingMachine.add_coins')
+    def test_add_coins_should_be_called_when_coin_is_valid(self, add_coins_):
+        self.vm.valid_coins = {'a': 1, 'b': 2}
+
+        self.vm.input_coin('a')
+
+        assert add_coins_.called
+
+    @mock.patch('vendingMachine.VendingMachine.add_coins')
+    def test_add_coins_should_be_able_to_handle_empty_string(self, add_coins_):
+        self.vm.valid_coins = {'a': 1, 'b': 2}
+
+        self.vm.input_coin('')
+
+        assert not add_coins_.called
 
 
 
